910cb788056dbac328d68b8e335757fb3c595f91
tamerlanchik <a.kochnov@internet.ru>
2023-01-19 15:07:23 +0300

    Добавил стат анализа

--- misc/статистическая обработка/stat1.m
+++ misc/статистическая обработка/stat1.m
@@ -18,23 +18,159 @@ for i=1:experiments_count
 end
 simout = parsim(simIn);
 %%
-save('batch_simulation_data_100_position_10sec.mat', 'simout', 'experiments_count');
+% save('batch_simulation_data_100_position_10sec.mat', 'simout', 'experiments_count');
 % parfor i=1:experiments_count
 %     res = sim("simulink/run_quadrotor_2020a.slx", "StopTime", '2', "Debug", "off");
 %     outputs = res.yout.signals;
 %     data{i} = outputs;
 % end
 %%
-load('batch_simulation_data_200_speed_5ms_20sec.mat')
+load('batch_simulation_data_200_landings_14sec.mat')
 %% Experiment 1. Время и точность посадки на подвижную цель
 % experiments_count = 50;
 t_end = zeros(experiments_count, 1);
+delta_r = zeros(experiments_count, 1);
+energy = zeros(experiments_count, 1);
 for i = 1:experiments_count
     t_end(i) = simout(i).yout.time(end);
+
+    signals = simout(i).yout.signals;
+    state = signals(1).values;
+    target = signals(5).values;
+    motors = signals(4).values;
+
+    r = state(end, [1, 3]);
+    r_target = target(end, [1, 3]);
+    delta_r(i) = sqrt(sum((r - r_target).^2));
+
+    e = 0;
+    for j = 1:length(simout(i).yout.time)
+        e = e + sum(motors(j, :).^2);
+    end
+    energy(i) = e;
 end
-t_end
+
+close all
+clc
+figure();
+subplot(1,3,1)
+
 histogram(t_end, 20)
+title("Время посадки")
+xlabel("t, c")
+disp("t_end")
 stat_analysis(t_end);
+
+subplot(1,3,2)
+histogram(delta_r, 20)
+title("Промах")
+xlabel("\Delta r, м")
+disp("delta_r")
+% stat_analysis(delta_r);
+
+subplot(1,3,3)
+histogram(energy, 50)
+title("Энергозатраты")
+xlabel("unit")
+disp("energy")
+% stat_analysis(energy);
+
+%% Стабилизация скорости
+load('batch_simulation_data_200_speed_5ms_10sec.mat')
+delta_v_max = zeros(experiments_count, 1);
+energy = zeros(experiments_count, 1);
+t_cut = 2;
+v_target = 5;
+
+for i = 1:experiments_count
+    t = simout(i).yout.time;
+
+    signals = simout(i).yout.signals;
+    state = signals(1).values(t>t_cut, :);
+    target = signals(5).values(t>t_cut, :);
+    motors = signals(4).values(t>t_cut, :);
+
+    delta_v_max(i) = max(state(:, 4) - v_target);
+
+    e = 0;
+    for j = 1:size(motors, 1)
+        e = e + sum(motors(j, :).^2);
+    end
+    energy(i) = e;
+end
+i = 5;
+signals = simout(i).yout.signals;
+state = signals(1).values(t>t_cut, :);
+target = signals(5).values(t>t_cut, :);
+motors = signals(4).values(t>t_cut, :);
+delta_v_single = state(:, 4) - v_target;
+
+close all
+clc
+figure();
+subplot(1,3,1)
+histogram(delta_v_max, 20)
+title("Максимальная ошибка скорости")
+xlabel("\Delta v, м/c")
+disp("delta_v_max")
+stat_analysis(delta_v_max);
+
+subplot(1,3,2)
+histogram(delta_v_single, 20)
+title("Ошибка скорости одного опыта")
+xlabel("\Delta v, м")
+disp("delta_v_single")
+stat_analysis(delta_v_single);
+
+subplot(1,3,3)
+histogram(energy, 20)
+title("Энергозатраты")
+xlabel("units")
+disp("energy")
+stat_analysis(energy);
+
+%% Стабилизация положения
+load('batch_simulation_data_100_position_10sec.mat')
+delta_r_max = zeros(experiments_count, 1);
+delta_r_end = zeros(experiments_count, 1);
+delta_v_max = zeros(experiments_count, 1);
+
+for i = 1:experiments_count
+    t_end(i) = simout(i).yout.time(end);
+
+    signals = simout(i).yout.signals;
+    state = signals(1).values;
+
+    r = state(end, 1);
+    delta_r_end(i) = r - 0;
+    delta_r_max(i) = max(state(:, 1) - 0);
+    delta_v_max(i) = max(state(:, 4) - 0);
+end
+
+close all
+clc
+figure();
+subplot(1,3,1)
+histogram(delta_r_end, 20)
+title("Конечное смещение")
+xlabel("\Delta x, м")
+disp("delta_r_end")
+stat_analysis(delta_r_end);
+
+subplot(1,3,2)
+histogram(delta_r_max, 20)
+title("Максимальное смещение")
+xlabel("\Delta r, м")
+disp("delta_r_max")
+stat_analysis(delta_r_max);
+
+subplot(1,3,3)
+histogram(delta_v_max, 20)
+title("Максимальная ошибка скорости")
+xlabel("\Delta v, м/с")
+disp("delta_v_max")
+stat_analysis(delta_v_max);
+
 %% Копипаста с Чулина для адаптации
 
 function stat_analysis(y)
@@ -54,13 +190,14 @@ m4x = mean((y - mx).^4) % Выборочный центральный момен
 gamma1 = m3x/(m2x^1.5) % Асимметрия, с. 83 книги
 
 % fprintf('  Эксцесс (для норм.распределения 3)\n')
-gamma2 = m4x/(m2x^2)  % Эксцесс, с. 86 книги
+gamma2 = m4x/(m2x^2) - 3  % Эксцесс, с. 86 книги
 
 % fprintf('  Контрэксцесс (для норм.распределения ~0.577)\n')
 gamma05 = m2x/sqrt(m4x)  % Контрэксцесс
 
+l = length(y);
 t_95 = 1.96;
-% delta_x_0.95 = t_95 * s2x / 
+delta_x_095 = t_95 * s2x / sqrt(l)
 % input('Нажмите ENTER');
 % %Для протокола
 % if nmXY=='x'
